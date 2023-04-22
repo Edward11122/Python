9da178eaeb88aa3df51cc0cc2f9878736ed75a5c
Edward11122 <126356312+Edward11122@users.noreply.github.com>
2023-04-22 11:55:50 -0700

    Game

---
+++ Game/test.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Edward")
+
+running = True
+while running:
+    pass
+
+pygame.quit()
